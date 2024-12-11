c9ec79ac322b00f315ff40b05cf4dbe35bba88a9
yayateayayatea <yayatea1046@gmail.com>
2024-12-11 10:01:12 +0800

    Create main.cpp
    start the file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+
+class pgame{
+  private:
+    string name;
+    char gender;
+  public:
+    void set_name(const string &name){
+      this-> name = name;
+    }
+    string get_name(){
+      return name;
+    }
+};
+int 
